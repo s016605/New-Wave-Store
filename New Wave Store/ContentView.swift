3232a17ccf8965d1f134e79fc412955156374b24
Zach Bader <s016605@students.lmsd.org>
2022-11-18 13:08:20 -0500

    Initial Commit

---
+++ New Wave Store/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  New Wave Store
+//
+//  Created by Zachary Bader (student LM) on 11/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ New Wave Store/New_Wave_StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  New_Wave_StoreApp.swift
+//  New Wave Store
+//
+//  Created by Zachary Bader (student LM) on 11/18/22.
+//
+
+import SwiftUI
+
+@main
+struct New_Wave_StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
